070836bb17a377f256ef5261c5639202ce8d0a97
Kavia AI <production@kavia.ai>
2026-03-23 04:13:48 +0000

    Checkpoint 1 - cgb00e172e

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/beacon-safe-monitoring-system-335801-335816/frontend_client
+npm run build
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
